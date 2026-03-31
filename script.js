d485ef9921522d7725e4655427a2a9e064491557
unknown <vishakhaburkul@gmail.com>
2026-03-31 15:33:58 +0530

    added local storage

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function showTasks() {
+  let list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    let li = document.createElement("li");
+    li.innerText = task;
+
+    let btn = document.createElement("button");
+    btn.innerText = "Delete";
+    btn.onclick = function () {
+      deleteTask(index);
+    };
+
+    li.appendChild(btn);
+    list.appendChild(li);
+  });
+}
+
 function addTask() {
   let input = document.getElementById("taskInput");
   let task = input.value;
 
   if (task === "") return;
 
-  let li = document.createElement("li");
-  li.innerText = task;
-
-  let btn = document.createElement("button");
-  btn.innerText = "Delete";
-  btn.onclick = function () {
-    li.remove();
-  };
-
-  li.appendChild(btn);
-
-  document.getElementById("taskList").appendChild(li);
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
   input.value = "";
+  showTasks();
 }
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  showTasks();
+}
+
+showTasks();
